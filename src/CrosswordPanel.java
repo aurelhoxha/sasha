1c5a994140737641221eb280dfa91f64103bd5d7
albjon <albjon.gjuzi@ug.bilkent.edu.tr>
2018-10-31 18:09:54 +0300

    Image Number

--- src/CrosswordPanel.java
+++ src/CrosswordPanel.java
@@ -10,18 +10,23 @@ public class CrosswordPanel extends JPanel{
 		selectedQuestion.setFont(new Font("Serif",Font.BOLD,14));
 		selectedQuestion.setAlignmentX(Component.LEFT_ALIGNMENT);
 		
-		GameInformation game = new GameInformation();
-		
+		//GameInformation game = new GameInformation();
 		
 		thePattern.setLayout(new GridLayout(5,5));
 		for(int i = 0; i < 25; i++ ) {
 			JTextField cellText = new JTextField("A");
+			cellText.setOpaque(false);
 			cellText.setHorizontalAlignment(JTextField.CENTER);
-			cellText.setFont(new Font("Serif",Font.PLAIN,13));
+			cellText.setFont(new Font("Serif",Font.PLAIN,33));
 			cellText.setAlignmentX(Component.CENTER_ALIGNMENT);
 			cellText.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-			thePattern.add(cellText);
+			JLabel cell = new JLabel(new ImageIcon("C:\\Users\\User\\Desktop\\BILKENT\\CS461\\Proj\\sasha\\img\\2.png"));
+			cell.setOpaque(true);
+			cell.setLayout(new BorderLayout());
+			cell.add(cellText);
+			thePattern.add(cell);
 		}
+		
 		setLayout(new BorderLayout());
 		add(selectedQuestion, BorderLayout.NORTH);
 		add(thePattern,BorderLayout.CENTER);
