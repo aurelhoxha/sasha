003e6f5d10f151e23fd39d1ec6d50aab6a07f774
albjon <albjon.gjuzi@ug.bilkent.edu.tr>
2018-11-01 18:44:35 +0300

    Old Dates

--- src/ButtonsPanel.java
+++ src/ButtonsPanel.java
@@ -1,4 +1,7 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 //Panel that Contains the Button of the Game
@@ -12,24 +15,61 @@ public class ButtonsPanel extends JPanel {
 	
 	//Constuctors
 	public ButtonsPanel() {
-		
 		//Set up the Components
 		//Adding Components to the Buttons Panel
 		solveButton = new JButton("Solve");
 		clearButton = new JButton("Clear");
 		revealButton = new JButton("Reveal");
-		showOtherButton = new JButton("Old Puzzles");
+		
+		String[] dates = new String[]{"Today","30 October 2018", "31 October 2018", "1 November 2018", "2 November 2018"};
+		JComboBox<String> others = new JComboBox<>(dates);
+		
+	    Dimension d = others.getPreferredSize();
+	    others.setPreferredSize(new Dimension(50, d.height));
+	    others.addActionListener(new ActionListener() {//Add actionlistner to listen for change
+	        public void actionPerformed(ActionEvent e) {
+
+	            String s = (String) others.getSelectedItem();//get the selected item
+
+	            switch (s) {//check for a match
+	                case "Today":
+	                    Test.selection = "https://www.nytimes.com/crosswords/game/mini";
+	                    break;
+	                case "30 October 2018":
+	                	Test.selection = "30october2018";
+	                    break;
+	                case "31 October 2018":
+	                	Test.selection = "31october2018";
+	                    break;
+	                case "1 November 2018":
+	                	Test.selection = "1november2018";
+	                    break;
+	                case "2 November 2018":
+	                	Test.selection = "2november2018";
+	                    break;
+
+	            }
+	        }
+	    });
+	    
+	    
+	    
+	    
+		
+		//showOtherButton = new JButton("Old Puzzles");
 		setLayout(new BoxLayout(this,BoxLayout.LINE_AXIS));
 		setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
 		add(Box.createHorizontalGlue());
+		add(Box.createRigidArea(new Dimension(100, 0)));
 		add(solveButton);
 		add(Box.createRigidArea(new Dimension(10, 0)));
 		add(clearButton);
 		add(Box.createRigidArea(new Dimension(10, 0)));
 		add(revealButton);
 		add(Box.createRigidArea(new Dimension(10, 0)));
-		add(showOtherButton);
-		
+		add(others);
+		add(Box.createRigidArea(new Dimension(100, 0)));		
 	}
-
+	
+	
 }
--- src/Test.java
+++ src/Test.java
@@ -5,52 +5,58 @@ import javax.swing.*;
 
 public class Test extends JFrame{
 
-	public static void main(String[] args) throws Exception {
-		
+	static String selection = "https://www.nytimes.com/crosswords/game/mini";
+	static String oldSelection = "";
+	public static void main(String[] args) throws Exception {	
 		//Variables to save the data of the game
 		Integer[] myClueNumber = new Integer[25];
 		ArrayList<String> myAcrossClues = new ArrayList<String>();
 		ArrayList<String> myDownClues = new ArrayList<String>();
 		String gameDay;
 		String gameDate;
-		
-		//Get Game Information
-		GameInformation myGame = new GameInformation("30october2018");
-		myGame.scrapeClueNumbers();
-		myGame.scrapeAcrossClues();
-		myGame.scrapeDownClues();
-		
-		//Initialize the Variables according to the Game Information
-		myClueNumber = myGame.getClueNumbers();
-		myAcrossClues = myGame.getAcrossClues();
-		myDownClues = myGame.getDownClues();
-		gameDay = myGame.getGameDay();
-		gameDate = myGame.getGameDate();
-		
+		GameInformation myGame;
 		
 		//Creation of the Game
-		JFrame myGameFrame = new JFrame("Sasha");
+		JFrame myGameFrame = new JFrame("Sasha");;
 		
-		//Initialization of the Main Panels
-		TopPanel myTopPanel = new TopPanel(gameDay, gameDate);
-		ButtonsPanel myButtonsPanel = new ButtonsPanel();
-		CenterPanel myCenterPanel = new CenterPanel(myAcrossClues, myDownClues, myClueNumber);
+		while(true){
+			if(!selection.equals(oldSelection)){
+				myGameFrame.dispose();
+				myGameFrame = new JFrame("Sasha");
+				//Get Game Information
+				//myGameFrame.dispose();
+				myGame = new GameInformation(selection);
+				myGame.scrapeClueNumbers();
+				myGame.scrapeAcrossClues();
+				myGame.scrapeDownClues();
+				
+				//Initialize the Variables according to the Game Information
+				myClueNumber = myGame.getClueNumbers();
+				myAcrossClues = myGame.getAcrossClues();
+				myDownClues = myGame.getDownClues();
+				gameDay = myGame.getGameDay();
+				gameDate = myGame.getGameDate();
+				//Initialization of the Main Panels
+				TopPanel myTopPanel = new TopPanel(gameDay, gameDate);
+				ButtonsPanel myButtonsPanel = new ButtonsPanel();
+				CenterPanel myCenterPanel = new CenterPanel(myAcrossClues, myDownClues, myClueNumber);
 		
-		//Setting Layout and Adding the Panels
-		myGameFrame.setLayout(new BorderLayout());
-		myGameFrame.add(myTopPanel,BorderLayout.NORTH);
-		myGameFrame.add(myButtonsPanel,BorderLayout.SOUTH);
-		myGameFrame.add(myCenterPanel,BorderLayout.CENTER);
-		myGameFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		myGameFrame.setSize(950, 650);
-		myGameFrame.setLocationRelativeTo(null);
-		myGameFrame.setVisible(true);
-		myGameFrame.setResizable(false);
-		
-		
-		
-		
+				//Setting Layout and Adding the Panels
+				myGameFrame.setLayout(new BorderLayout());
+				myGameFrame.add(myTopPanel,BorderLayout.NORTH);
+				myGameFrame.add(myButtonsPanel,BorderLayout.SOUTH);
+				myGameFrame.add(myCenterPanel,BorderLayout.CENTER);
+				myGameFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+				myGameFrame.setSize(950, 650);
+				myGameFrame.setLocationRelativeTo(null);
+				myGameFrame.setVisible(true);
+				myGameFrame.setResizable(false);
+				
+				oldSelection = selection;
+			}
+			System.out.println("");
+			//System.out.println(selection + " " + oldSelection);
+		}
 		
 	}
-
 }
