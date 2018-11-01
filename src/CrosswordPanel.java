5f0a9caf8312241016018c8307f98fcbeaab085b
onur-elbirlik <onur.elbirlik@gmail.com>
2018-11-01 16:14:42 +0300

    Limited user for entering one letter per box

--- src/CrosswordPanel.java
+++ src/CrosswordPanel.java
@@ -1,4 +1,8 @@
+import javafx.scene.control.Cell;
+
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 import javax.swing.*;
@@ -12,33 +16,32 @@ public class CrosswordPanel extends JPanel{
 		selectedQuestion.setFont(new Font("Serif",Font.BOLD,14));
 		selectedQuestion.setAlignmentX(Component.LEFT_ALIGNMENT);
 
-		
+
 		JTextField[] cellText = new JTextField[25];
 		thePattern.setLayout(new GridLayout(5,5));
-		for(int i = 0; i < myClueNumber.length; i++ ) {
-				if(myClueNumber[i] == -1){
-					cellText[i] = new JTextField();
-					cellText[i].setBackground(Color.BLACK);
-					cellText[i].setEditable(false);
-					cellText[i].setBorder(BorderFactory.createLineBorder(Color.gray));
-					thePattern.add(cellText[i]);
-				}
-				else {		
-					String cellimg = "./img/" + myClueNumber[i] + ".png";
-					cellText[i] = new JTextField();
-					cellText[i].setOpaque(false);
-					cellText[i].setHorizontalAlignment(JTextField.CENTER);
-					cellText[i].setFont(new Font("Helvetica",Font.PLAIN,33));
-					cellText[i].setAlignmentX(Component.CENTER_ALIGNMENT);
-					cellText[i].setBorder(BorderFactory.createLineBorder(Color.gray));				
-					JLabel cell = new JLabel(new ImageIcon(cellimg));
-					cell.setOpaque(true);
-					cell.setLayout(new BorderLayout());
-					cell.add(cellText[i]);
-					thePattern.add(cell);
-				}
-		}
-		
+		for(int i = 0; i < myClueNumber.length; i++ )
+			if (myClueNumber[i] == -1) {
+				cellText[i] = new JTextField();
+				cellText[i].setBackground(Color.BLACK);
+				cellText[i].setEditable(false);
+				cellText[i].setBorder(BorderFactory.createLineBorder(Color.gray));
+				thePattern.add(cellText[i]);
+			} else {
+				String cellimg = "./img/" + myClueNumber[i] + ".png";
+				cellText[i] = new JTextField();
+				cellText[i].setDocument(new JTextFieldLimit(1));
+				cellText[i].setOpaque(false);
+				cellText[i].setHorizontalAlignment(JTextField.CENTER);
+				cellText[i].setFont(new Font("Helvetica", Font.PLAIN, 33));
+				cellText[i].setAlignmentX(Component.CENTER_ALIGNMENT);
+				cellText[i].setBorder(BorderFactory.createLineBorder(Color.gray));
+				JLabel cell = new JLabel(new ImageIcon(cellimg));
+				cell.setOpaque(true);
+				cell.setLayout(new BorderLayout());
+				cell.add(cellText[i]);
+				thePattern.add(cell);
+			}
+
 		setLayout(new BorderLayout());
 		add(selectedQuestion, BorderLayout.NORTH);
 		add(thePattern,BorderLayout.CENTER);
