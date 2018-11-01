b7ef73427777ba274d8abb30ef5b7fd3680b80d7
onur-elbirlik <onur.elbirlik@gmail.com>
2018-11-01 19:05:54 +0300

    remove caret

--- src/ColorChange.java
+++ src/ColorChange.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.text.Caret;
 import java.awt.*;
 import java.awt.event.FocusListener;
 
@@ -7,14 +8,21 @@ public class ColorChange {
     public static FocusListener myFocusListener = new java.awt.event.FocusListener() {
         private final Color ACTIVE_COLOUR = Color.YELLOW;
         public void focusGained(java.awt.event.FocusEvent focusEvent) {
-                JTextField src = (JTextField)focusEvent.getSource();
-                src.setOpaque(true);
-                src.setBackground(ACTIVE_COLOUR);
+            JTextField src = (JTextField)focusEvent.getSource();
+            src.setOpaque(true);
+            src.setBackground(ACTIVE_COLOUR);
+            Caret caret = src.getCaret();
+            caret.setSelectionVisible(false);
+            caret.setVisible(false);
         }
 
         public void focusLost(java.awt.event.FocusEvent focusEvent) {
             JTextField src = (JTextField)focusEvent.getSource();
             src.setOpaque(false);
+
+            Caret caret = src.getCaret();
+            caret.setSelectionVisible(false);
+            caret.setVisible(false);
         }
     };
 }
