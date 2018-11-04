e578378914077806848fe46f142f17d860a15070
albjon <albjon.gjuzi@ug.bilkent.edu.tr>
2018-11-04 19:13:31 +0300

    if myClue[i] == 0 fixed

--- src/CrosswordPanel.java
+++ src/CrosswordPanel.java
@@ -18,8 +18,8 @@ public class CrosswordPanel extends JPanel{
 //		selectedQuestion.setAlignmentX(Component.LEFT_ALIGNMENT);
 
 		thePattern.setLayout(new GridLayout(5,5));
-		//int count = myClueNumber.length;
-		for(int i = 0; i < myClueNumber.length; i++ )
+		int fin = myClueNumber.length;
+		for(int i = 0; i < fin; i++ )
 			if (myClueNumber[i] == -1) {
 				cellText[i] = new JTextField();
 				UpperCaseDocument ucd = new UpperCaseDocument();
@@ -29,7 +29,6 @@ public class CrosswordPanel extends JPanel{
 				cellText[i].setBorder(BorderFactory.createLineBorder(Color.gray));
 				thePattern.add(cellText[i]);
 			} else {
-				String cellimg = "./img/" + myClueNumber[i] + ".png";
 				cellText[i] = new JTextField();
 				UpperCaseDocument ucd = new UpperCaseDocument();
 				cellText[i].setDocument(ucd);
@@ -93,11 +92,18 @@ public class CrosswordPanel extends JPanel{
 				cellText[i].setFont(new Font("Helvetica", Font.PLAIN, 33));
 				cellText[i].setAlignmentX(Component.CENTER_ALIGNMENT);
 				cellText[i].setBorder(BorderFactory.createLineBorder(Color.gray));
-				JLabel cell = new JLabel(new ImageIcon(cellimg));
-				cell.setOpaque(true);
-				cell.setLayout(new BorderLayout());
-				cell.add(cellText[i]);
-				thePattern.add(cell);
+				
+				if(myClueNumber[i] == 0){
+					thePattern.add(cellText[i]);
+				}
+				else {
+					String cellimg = "./img/" + myClueNumber[i] + ".png";
+					JLabel cell = new JLabel(new ImageIcon(cellimg));
+					cell.setOpaque(true);
+					cell.setLayout(new BorderLayout());
+					cell.add(cellText[i]);
+					thePattern.add(cell);
+				}
 
 			}
 
